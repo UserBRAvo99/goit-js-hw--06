9c77c6ed5a1b7852c16f8653c3c5dcdedb561ed4
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-18 19:44:02 +0200

    commit

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const categoriesEl = document.querySelector('#categories');
+console.log(`Number of categories: ${categoriesEl.children.length}`);
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
