e9ab4a1c9082b9f03a99fb6d763ec9910edffa7f
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-19 00:09:53 +0200

    4task

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,10 @@
 const counterValue = document.querySelector('#value');
+const incrementEl = document.querySelector('button[data-action="increment"]');
+const decrementEl = document.querySelector('button[data-action="decrement"]');
+
+incrementEl.addEventListener('click', () => {
+    counterValue.textContent = Number(counterValue.textContent) + 1;
+});
+decrementEl.addEventListener('click', () => {
+    counterValue.textContent = Number(counterValue.textContent) - 1;
+});
