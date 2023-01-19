5099c288a84333c5810fcd0c6c4bf7abc3c99eb7
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-19 17:54:59 +0200

    task7

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const sizeControlEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+sizeControlEl.addEventListener('input', event => {
+    textEl.style.fontSize = event.target.value + 'px';
+});
