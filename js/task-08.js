a635fdf7e0285f70b8bc58d63f922034415dcf81
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-21 18:18:45 +0200

    task9

--- js/task-08.js
+++ js/task-08.js
@@ -3,9 +3,10 @@ console.log(formEl);
 const userData = {};
 formEl.addEventListener('submit', event => {
     event.preventDefault();
-
+    // Варіант 1
     const emailEl = event.currentTarget.elements.email.value;
     const passwordEl = event.currentTarget.elements.password.value;
+    // Варіант 2
     // const {
     //     elements: { email, password },
     // } = event.currentTarget;
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnChangeColorEl = document.querySelector('.change-color');
+
+btnChangeColorEl.addEventListener('click', event => {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+    document.querySelector('.color').textContent =
+        document.querySelector('body').style.backgroundColor;
+});
